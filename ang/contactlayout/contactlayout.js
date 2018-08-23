8326d994a1bcb78f9689a2e789b87be2499520e1
Coleman Watts <coleman@civicrm.org>
2018-08-23 14:10:29 -0400

    Editable block titles

--- ang/contactlayout/contactlayout.js
+++ ang/contactlayout/contactlayout.js
@@ -272,7 +272,7 @@
 
     // Return the editable properties of a block
     function getBlockProperties(block) {
-      return _.pick(block, 'name', 'collapsible', 'collapsed', 'showTitle');
+      return _.pick(block, 'name', 'title', 'collapsible', 'collapsed', 'showTitle');
     }
 
     // Write layout data to the server
@@ -357,6 +357,48 @@
       $scope.newLayout();
     }
 
+  });
+
+  // Editable block titles using ngModel & html5 contenteditable
+  angular.module('contactlayout').directive("layoutBlockTitle", function() {
+    return {
+      restrict: "A",
+      require: "ngModel",
+      link: function(scope, element, attrs, ngModel) {
+        var ts = CRM.ts('contactlayout');
+
+        function read() {
+          var htmlVal = element.html();
+          if (!htmlVal) {
+            htmlVal = ts('Untitled');
+            element.html(htmlVal);
+          }
+          ngModel.$setViewValue(htmlVal);
+        }
+
+        ngModel.$render = function() {
+          element.html(ngModel.$viewValue || ' ');
+        };
+
+        // Special handling for enter and escape keys
+        element.on('keydown', function(e) {
+          // Enter: prevent line break and save
+          if (e.which === 13) {
+            e.preventDefault();
+            element.blur();
+          }
+          // Escape: undo
+          if (e.which === 27) {
+            element.html(ngModel.$viewValue || ' ');
+            element.blur();
+          }
+        });
+
+        element.on("blur change", function() {
+          scope.$apply(read);
+        });
+      }
+    };
   });
 
 })(angular, CRM.$, CRM._);
