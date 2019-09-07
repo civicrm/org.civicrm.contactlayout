a9ba417638c51280e4ea525b37394fe78ef6065b
Coleman Watts <coleman@civicrm.org>
2019-09-07 11:34:38 -0400

    Fix potential e-notice

--- CRM/Contactlayout/BAO/ContactLayout.php
+++ CRM/Contactlayout/BAO/ContactLayout.php
@@ -111,7 +111,7 @@ class CRM_Contactlayout_BAO_ContactLayout extends CRM_Contactlayout_DAO_ContactL
   public static function getBlock($fullName) {
     list($groupName, $blockName) = explode('.', $fullName, 2);
     $group = CRM_Utils_Array::value($groupName, self::getAllBlocks());
-    foreach ($group['blocks'] as $block) {
+    foreach (CRM_Utils_Array::value('blocks', $group, []) as $block) {
       if ($block['name'] == $fullName) {
         return $block;
       }
