3130211ec7b21483ee8c927ba9ee79447c45badb
benjamin <ben@civicrm.org>
2025-01-17 15:13:53 +0000

    dev/core#5688 set array keys smarty tab array to work with TabHeader.tpl

--- contactlayout.php
+++ contactlayout.php
@@ -103,8 +103,8 @@ function contactlayout_civicrm_pageRun(&$page) {
             $tabs[$id]['icon'] = $tab['icon'] ?? $tabs[$id]['icon'] ?? NULL;
           }
         }
-        usort($tabs, ['CRM_Utils_Sort', 'cmpFunc']);
-        $page->assign('allTabs', array_values($tabs));
+        uasort($tabs, ['CRM_Utils_Sort', 'cmpFunc']);
+        $page->assign('allTabs', $tabs);
       }
       if (CRM_Core_Permission::check('administer CiviCRM')) {
         CRM_Core_Region::instance('contact-actions-ribbon')
